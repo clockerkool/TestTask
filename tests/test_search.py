e4ec8cd0ea28049b86fb028a2663686ef7824257
clockerkool <105645239+clockerkool@users.noreply.github.com>
2024-06-10 17:36:20 +0500

    splitting tests by files

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,43 +1,9 @@
 import re
 import requests
 from configuration import *
-from utils import get_artwork_response, get_department_displayName
-from src.enums.global_enums import GlobalErrorMessages
-from src.pydantic_schemas.artworks import ArtWork
+from utils import get_artwork_response
 from src.pydantic_schemas.search_schema import Search
-from src.pydantic_schemas.objects_id import ObjectsIDs
-from src.pydantic_schemas.departments import Departments
 from src.baseclasses.response import Response
-
-
-def test_api_request():
-    """ Проверка, что API возвращает корректный статус-код """
-    response = requests.get(url=SERVICE_URL)
-    assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
-
-
-def test_objects():
-    """Проверка Objects (Возврат всех допуснимых идентификаторов объектов"""
-    response = requests.get(url=TEST_OBJECTS_URL)
-    all_artworks = Response(response)
-    all_artworks.assert_status_code(200)
-    all_artworks.validate(ObjectsIDs)
-
-
-def test_artwork():
-    """Проверка объекта произведения искусства."""
-    response = requests.get(url=TEST_ARTWORK_URL + "5")
-    artwork = Response(response)
-    artwork.assert_status_code(200)
-    artwork.validate(ArtWork)
-
-
-def test_departments():
-    """Проверка отделов произведений искусств"""
-    response = requests.get(url=TEST_DEPARTMENTS_URL)
-    departments = Response(response)
-    departments.assert_status_code(200)
-    departments.validate(Departments)
 
 
 def test_search_query():
@@ -108,7 +74,7 @@ def test_search_hasImages():
 
 def test_search_dates():
     """
-    Проверка поиска с парамтрами диапазона дат
+    Проверка поиска с парамтрами диапазона дат (ТЕСТ ОТНОСИТЕЛЬНО ДОЛГИЙ)
     P.S В какой-то момент не приходит страница обекта произведения искусства (код 404)
     решил просто пропускать этот несуществующий объект
     """
